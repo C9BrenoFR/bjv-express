e14da1c9f8782e07c7ba31816a195d930b602ce9
C9BrenoFR <brenufr@gmail.com>
2025-07-21 21:51:18 -0300

    Cria helper para dados enum

---
+++ app/Helpers/enum_data.php
@@ -0,0 +1,15 @@
+<?php
+
+class Roles
+{
+    public static $ADMIN = 0;
+    public static $OPERATOR = 1;
+    public static $DELIVER = 2;
+}
+
+class Status
+{
+    public static $IN_SEPARATION = 0;
+    public static $ON_WAY = 1;
+    public static $DELIVERED = 2;
+}
