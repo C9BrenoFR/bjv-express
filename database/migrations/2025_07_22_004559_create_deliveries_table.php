e1a7f868178362389e25a8ac3672b69d2100caa8
C9BrenoFR <brenufr@gmail.com>
2025-07-21 21:51:29 -0300

    Modelagem de banco de dados

---
+++ database/migrations/2025_07_22_004559_create_deliveries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', [0, 1, 2]);
+            $table->string('step');
+            $table->foreignId('package_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('last_to_update')->constrained('users');
+            $table->foreignId('unit_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deliveries');
+    }
+};
