949d8546db9f5f5f146edafc8683b78be57a4ac5
C9BrenoFR <brenufr@gmail.com>
2025-07-28 20:06:54 -0300

    Refactor enum classes to check for existence before declaration

--- app/Helpers/enum_data.php
+++ app/Helpers/enum_data.php
@@ -1,15 +1,19 @@
 <?php
 
-class Roles
-{
-    public const ADMIN = '0';
-    public const OPERATOR = '1';
-    public const DELIVER = '2';
-}
+if (!class_exists('Roles')) {
+    class Roles
+    {
+        public const ADMIN = '0';
+        public const OPERATOR = '1';
+        public const DELIVER = '2';
+    }
 
-class Status
-{
-    public const IN_SEPARATION = '0';
-    public const ON_WAY = '1';
-    public const DELIVERED = '2';
+}
+if (!class_exists('Status')) {
+    class Status
+    {
+        public const IN_SEPARATION = '0';
+        public const ON_WAY = '1';
+        public const DELIVERED = '2';
+    }
 }
