9f624cfbec908dec192f1207faf8e4fb3b25f0ba
C9BrenoFR <brenufr@gmail.com>
2025-07-28 20:07:13 -0300

    Refactor route middleware to use specific middleware classes for better clarity and organization

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\DeliverMiddleware;
+use App\Http\Middleware\OperatorMiddleware;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -7,11 +10,29 @@ Route::get('/', function () {
     return redirect('/login');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(AdminMiddleware::class)->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+Route::middleware(DeliverMiddleware::class)->group(function () {
+    Route::get('/deliver', function () {
+        echo "Logado como Entregador";
+        return;
+    })->name('dashboard.deliver');
+});
+
+Route::middleware(OperatorMiddleware::class)->group(function () {
+    Route::get('/operator', function () {
+        echo "Logado como operador";
+        return;
+    })->name('dashboard.operator');
+});
+
+Route::get('/test-auth', function () {
+    dd(Auth::user());
+})->middleware('web');
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
