45a006586d29f12bc4761dc8cadb23b001b0863a
C9BrenoFR <brenufr@gmail.com>
2025-07-29 02:31:47 -0300

    Fix visibility of constants in Address model and improve print method formatting

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -17,18 +17,18 @@ class Address extends Model
         'complement',
     ];
 
-    static const TOTAL = 0;
-    static const UPPER_TO_CITY = 1;
-    static const STREET_MAIN_INFORMATION = 2;
+    public const TOTAL = 0;
+    public const UPPER_TO_CITY = 1;
+    public const STREET_MAIN_INFORMATION = 2;
     public function print(int $config): string
     {
         if ($config == 0) {
-            return $this->street . ' ,' . $this->number . ' ' . $this->complement . ' ,'
+            return $this->street . ' ,' . $this->number . ' ,' . ($this->complement ? $this->complement . ' ,' : '')
                 . $this->neighborhood . ' ,' . $this->city . ' ,' . $this->state . ' ,' . $this->country;
         } else if ($config == 1) {
             return $this->city . ' ,' . $this->state . ' ,' . $this->county;
         } else if ($config == 2) {
-            return $this->street . ' ,' . $this->number . ' ,' . $this->complement . ' ,' . $this->neighborhood;
+            return $this->street . ' ,' . $this->number . ' ,' . ($this->complement ? $this->complement . ' ,' : '') . $this->neighborhood;
         }
         return $this->city;
     }
