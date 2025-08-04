b347a125b482463c73b2702d3845f1cc3f8f72d1
C9BrenoFR <brenufr@gmail.com>
2025-08-04 13:11:29 -0300

    Add update functionality for employee management routes

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,23 @@ Route::middleware(AdminMiddleware::class)->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
+    // Rotas de pacotes
     Route::get('admin/packages', [PackageController::class, 'index'])->name('admin.packages');
+    Route::get('admin/packages/create', [PackageController::class, 'create'])->name('admin.packages.create');
+    Route::post('admin/packages', [PackageController::class, 'store'])->name('admin.packages.store');
+    Route::get('admin/packages/view/{package}', [PackageController::class, 'show'])->name('admin.packages.show');
+    Route::get('admin/packages/edit/{package}', [PackageController::class, 'edit'])->name('admin.packages.edit');
+    Route::put('admin/packages/{package}', [PackageController::class, 'update'])->name('admin.packages.update');
+    Route::delete('admin/packages/{package}', [PackageController::class, 'destroy'])->name('admin.packages.destroy');
 
+    // Rotas de funcionários
     Route::get('admin/employees', [EmployeeController::class, 'index'])->name('admin.employees');
+    Route::get('admin/employees/create', [EmployeeController::class, 'create'])->name('admin.employees.create');
+    Route::post('admin/employees', [EmployeeController::class, 'store'])->name('admin.employees.store');
+    Route::get('admin/employees/view/{user}', [EmployeeController::class, 'show'])->name('admin.employees.show');
+    Route::get('admin/employees/edit/{user}', [EmployeeController::class, 'edit'])->name('admin.employees.edit');
+    Route::put('admin/employees/{user}', [EmployeeController::class, 'update'])->name('admin.employees.update');
+    Route::delete('admin/employees/{user}', [EmployeeController::class, 'destroy'])->name('admin.employees.destroy');
 });
 
 Route::middleware(DeliverMiddleware::class)->group(function () {
