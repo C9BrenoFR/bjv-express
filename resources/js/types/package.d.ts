3a2e683c645307bb32629827df177f4ec4d9019c
C9BrenoFR <brenufr@gmail.com>
2025-08-06 15:44:13 -0300

    Add PackageType interface definition for package details

---
+++ resources/js/types/package.d.ts
@@ -0,0 +1,14 @@
+export interface PackageType {
+    id: number;
+    code: string;
+    width: number;
+    height: number;
+    depth: number;
+    weight: number;
+    formatted_address: string;
+    status: string;
+    step: string;
+    unit_title: string;
+    created_at: string;
+    updated_at: string;
+}
